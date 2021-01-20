0f5aa9756b3130ea7e1cccdd3415aea55cd04283
bronz57 <{chbronz88}@gmail.com>
2021-01-20 11:19:11 +0100

    Primo commit

---
+++ bronzetti.christian.4h.EsData/Models/Data.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bronzetti.christian._4h.EsData.Models
+{
+    class Data
+    {
+        int _giorno;
+        int _mese;
+        int _anno;
+
+        public int Giorno
+        { 
+            get
+            {
+                return _giorno;
+            }
+
+            set
+            {
+                _giorno = value;
+            }
+        }
+        public int Mese
+        {
+            get
+            {
+                return _mese;
+            }
+
+            set
+            {
+                _mese = value;
+            }
+        }
+        public int Anno 
+        {
+            get
+            {
+                return _anno;
+            }
+
+            set
+            {
+                _anno = value;
+            }
+        }
+
+        public Data(int g, int m, int a)
+        {
+            _giorno = g;
+            _mese = m;
+            _anno = a;
+        }
+
+        public string Out(Data d)
+        {
+            string g = d._giorno.ToString();
+            string m = d._mese.ToString();
+            string a = d._anno.ToString();
+
+            if (d._giorno < 10)
+                g = "0" + g;
+
+            if (d._mese < 10)
+                m = "0" + m;
+
+            if (d._anno < 10)
+                a = "0" + a;
+
+            return $"{g}/{m}/{a}";
+
+        }
+
+
+        public void Mod(int g, int m, int a)
+        {
+            //alla chiamata di mod modifico la vada che però deve essere corretta quindi la proprietà e non il campo private
+            Giorno = g; 
+            Anno = a;
+            Mese = m;
+        }
+  
+        public bool Val(Data d)
+        {
+            bool f = true;  //flag di controllo
+
+            if (d._anno>= 0) //se minore di 0 sarebbe avanti cristo ? ;D
+                f = true;
+            
+            if(d._mese > 0 && d._mese < 13) //controla se il mese è compreso tra 1 e 12
+                f = true;
+
+            if (d._giorno > 0 && d._giorno < 32) //se è compreso tra 1 e 31
+                f = true;
+
+            if (d._mese == 2 && d._giorno > 28)
+                f = false;
+            
+            
+            if (d._giorno == 29 && d._mese == 2)
+            {
+                if (ControlloBisestile(d._anno) == true && d._mese == 2 && d._giorno == 29)
+                    f = true;
+            }
+           
+                
+                
+                   
+                            
+
+            return f;
+        }
+
+        private bool ControlloBisestile(int a)
+        {
+
+            if (a % 400 == 0 || (a % 4 == 0 && a % 100 != 0))
+                return true;
+            else
+                return false;
+        }
+
+
+
+    }
+}
---
+++ bronzetti.christian.4h.EsData/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using bronzetti.christian._4h.EsData.Models;
+
+namespace bronzetti.christian._4h.EsData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programma classe Data scritto da Christian Bronzetti, 4h");
+
+            Data d = new Data(28, 08, 01);
+
+            Console.WriteLine($"Data originale {d.Out(d)}");
+
+            d.Mod(28, 03, 2015);
+
+            Console.WriteLine($"Data modificata {d.Out(d)}");
+
+            d.Mod(31, 02, 2019);
+
+            Console.WriteLine($"Data da verificare {d.Out(d)}");
+
+            if (d.Val(d))
+                Console.WriteLine("La data è valida");
+            else
+                Console.WriteLine("La data è errata");
+
+            Data data1 = new Data(31, 02, 1992);
+
+            Console.WriteLine($"Data 2 da verificare {data1.Out(data1)}");
+
+            if (data1.Val(data1))
+                Console.WriteLine("La data è valida");
+            else
+                Console.WriteLine("La data è errata");
+
+
+        }
+    }
+}
